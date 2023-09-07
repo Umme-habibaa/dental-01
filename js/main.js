346580c9b949f28c408040fa707f4b1ccd910243
Umme-habibaa <138841395+Umme-habibaa@users.noreply.github.com>
2023-09-07 08:51:57 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(".banner_carousel").owlCarousel({
+        items:1,
+        nav:true,
+        loop:true,
+        navText:['<span><i class="fa-solid fa-chevron-left"></i></span>', '<span><i class="fa-solid fa-chevron-right"></i></span>']
+    });
+  });
+  
+
+  $(document).ready(function(){
+    $(".happy_carousel").owlCarousel({
+        items:5,
+        nav:true,
+        loop:true,
+        navText:['<span><i class="fa-solid fa-chevron-left"></i></span>', '<span><i class="fa-solid fa-chevron-right"></i></span>']
+    });
+  });
+  
